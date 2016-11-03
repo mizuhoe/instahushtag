d95606bb8f6bdb4a2bb04c704b6a973576eeb33b
江森みずほ <MIzuho@emorimizuho-no-macbook-air.local>
2016-11-03 19:23:33 +0900

    Initial Commit

---
+++ instahushtag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instahushtag
+//
+//  Created by 江森みずほ on 2016/11/03.
+//  Copyright © 2016年 江森みずほ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
